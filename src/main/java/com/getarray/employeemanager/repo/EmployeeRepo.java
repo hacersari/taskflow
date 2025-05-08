0ad10e2ceb99158e616eba53c05f33d4a323f43d
Aynur <hsari@cs.washington.edu>
2025-05-08 11:27:49 -0700

    Service class done

--- src/main/java/com/getarray/employeemanager/repo/EmployeeRepo.java
+++ src/main/java/com/getarray/employeemanager/repo/EmployeeRepo.java
@@ -3,6 +3,11 @@ package com.getarray.employeemanager.repo;
 import com.getarray.employeemanager.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 // long is the type of primary key "id"
 public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+    void deleteEmployeeById(Long id); // query naming convention, it will delete the employee by matching the id (spring)
+
+    Optional findEmployeeById(Long id);
 }
--- src/main/java/com/getarray/employeemanager/service/EmployeeService.java
+++ src/main/java/com/getarray/employeemanager/service/EmployeeService.java
@@ -5,6 +5,7 @@ import com.getarray.employeemanager.repo.EmployeeRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 
@@ -21,6 +22,27 @@ public class EmployeeService {
         // create this employee in actual database
         employee.setEmployeeCode(UUID.randomUUID().toString());
         return employeeRepo.save(employee);
+    }
+
+    // to return the list of all  the employees
+    public List<Employee> findAllEmployees(){
+        return employeeRepo.findAll();
+    }
+
+    public Employee updateEmployee(Employee employee){
+        return employeeRepo.save(employee);
+    }
+
+
+    // find an employee by id
+    public Employee findEmployeeById(Long id){
+        return employeeRepo.findEmployeeById(id)
+                .orElseThrow(() -> new UserNotFoundException ("User by Id " +id+ " was not found"));
+    }
+
+    public void deleteEmployee(Long id){
+        employeeRepo.deleteEmployeeById(id);
+
     }
 
 }
